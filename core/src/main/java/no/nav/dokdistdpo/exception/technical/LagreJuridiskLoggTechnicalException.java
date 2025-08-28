46e570edbeeca0aa7be356798a43a4ce0f2fc76e
Anders Opskar Voldsund <anders.voldsund@nav.no>
2025-08-28 15:30:09 +0200

    MMA-8239 Legg på retry ved read timeouts mot juridisk logg
    Oppdateringa av juridisk logg skjer etter at meldinga er sendt til DPO. Dersom juridisk logg feilar vil det hindre at statusen i dokdistadmin blir oppdatert, og i tillegg hamnar meldinga på BOQ som seinare blir resendt.
    
    Retry kan hindre at det blir sendt duplikate sendingar til DPO.

--- core/src/main/java/no/nav/dokdistdpo/exception/technical/LagreJuridiskLoggTechnicalException.java
+++ core/src/main/java/no/nav/dokdistdpo/exception/technical/LagreJuridiskLoggTechnicalException.java
@@ -1,7 +1,13 @@
 package no.nav.dokdistdpo.exception.technical;
 
 public class LagreJuridiskLoggTechnicalException extends DokdistdpoTechnicalException {
+
 	public LagreJuridiskLoggTechnicalException(String message) {
 		super(message);
 	}
+
+	public LagreJuridiskLoggTechnicalException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
 }
