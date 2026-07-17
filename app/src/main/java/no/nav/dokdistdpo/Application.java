1bcd35221dd3572b5e2a3f54be631a4796b3e422
Anders Opskar Voldsund <anders.voldsund@nav.no>
2026-07-17 11:46:07 +0200

    MMA-8777 Oppgrader avhengigheiter
    Fiks litt forslag frå IntelliJ

--- app/src/main/java/no/nav/dokdistdpo/Application.java
+++ app/src/main/java/no/nav/dokdistdpo/Application.java
@@ -22,7 +22,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
